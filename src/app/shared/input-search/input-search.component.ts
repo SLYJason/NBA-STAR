a0c13f79c67f0761fbbd9bb081c2f7e2964062ea
Luyi Song <songly91.6.1@gmail.com>
2018-04-12 21:47:03 -0400

    add shared searchbox

--- src/app/shared/input-search/input-search.component.ts
+++ src/app/shared/input-search/input-search.component.ts
@@ -3,15 +3,24 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-input-search',
   template: `
-    <div class="form-group">
-      <label for="player">Name:</label>
-      <input type="text" class="form-control" id="player">
+    <div>
+      <label for="player"></label>
+      <input type="text" id="player" placeholder="Type...">
     </div>
   `,
   styles: [`
     input[type="text"] {
       outline: none;
-      width: 30%;
+      width: 20%;
+      height: 2.5rem;
+      border: 1px solid #000000;
+      border-radius: .5rem;
+      text-indent: .5rem;
+    }
+    input[type="text"]::placeholder {
+      font-weight: bold;
+      font-size: 1.2rem;
+      text-indent: .5rem;
     }
   `]
 })
